11687ba697f5cb1e90beacfde926cb189f8287ef
Rasmus <morganmistrz@gmail.com>
2019-07-08 19:41:41 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import Discord
+
+
+TOKEN = 'NTk3MDM0MzA0ODU3NDQwMjU3.XSCNkg.gI9YzjTOwzz64jMaXZGvhibmo0k'
+
+client = Discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!hello'):
+        channel = message.channel
+        await channel.send('Say hello!')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
